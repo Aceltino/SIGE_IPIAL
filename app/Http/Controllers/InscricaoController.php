7f85478d1ac358cfc8cd3b6c697e70b5d2db749d
Aceltino Quianvo <118996385+Aceltino@users.noreply.github.com>
2023-05-31 23:47:05 +0100

    Basic change

--- app/Http/Controllers/InscricaoController.php
+++ app/Http/Controllers/InscricaoController.php
@@ -48,74 +48,74 @@ class InscricaoController extends Controller
             $msg="Lamentamos! Este número de telefone já existe em nossa base de dados, mas a candi";
             return redirect()->back()->with("ErroTelefone",$msg);
         }
-        echo "Aceltino";
-        // $dadosEscola = [
-        //     'nome_escola'=>$request->nome_escola,
-        //     'turno'=>$request->turno,
-        //     'num_processo'=>$request->num_processo,
-        //     'num_aluno'=>$request->num_aluno,
-        //     'ultimo_anoLectivo'=>$request->ultimo_anoLectivo,
-        //     'turma_aluno' =>$request->turma_aluno,
-        //     'ling_port'   => $request->LinguaP,
-        //     'matematica'  => $request->Matematic,
-        //     'fisica' =>  $request->Fisic,
-        //     'quimica' => $request->Quimic
-        // ];
-        // $idEscola = $this->storeEscola($dadosEscola);
-        // if(!$idEscola)
-        // {
-        //     $msg="Lamentamos! Certifique de que introduziu os dados correctos ou tente mais tarde...";
-        //     return redirect()->back()->with("ErroCadastro",$msg);
-        // }
 
-        // $idAnolectivo = $this->pegarAnoLectivo();
-        // if(!$idAnolectivo)
-        // {
-        //     $msg="Lamentamos! Dados não cadastrado, tente este processo mais tarde...";
-        //     return redirect()->back()->with("ErroCadastro",$msg);
-        // }
+        $dadosEscola = [
+            'nome_escola'=>$request->nome_escola,
+            'turno'=>$request->turno,
+            'num_processo'=>$request->num_processo,
+            'num_aluno'=>$request->num_aluno,
+            'ultimo_anoLectivo'=>$request->ultimo_anoLectivo,
+            'turma_aluno' =>$request->turma_aluno,
+            'ling_port'   => $request->LinguaP,
+            'matematica'  => $request->Matematic,
+            'fisica' =>  $request->Fisic,
+            'quimica' => $request->Quimic
+        ];
+        $idEscola = $this->storeEscola($dadosEscola);
+        if(!$idEscola)
+        {
+            $msg="Lamentamos! Certifique de que introduziu os dados correctos ou tente mais tarde...";
+            return redirect()->back()->with("ErroCadastro",$msg);
+        }
 
-        // $dadosCandidato=[
-        //     //Dados do candidato
-        //     'nome_pai_cand'=>$request->nome_pai_cand,
-        //     'nome_mae_cand'=>$request->nome_mae_cand,
-        //     'naturalidade_cand'=>$request->naturalidade_cand,
-        //     'status' => "Pendente",
-        //     'pessoa_id' => $idPessoa,
-        //     'ano_lectivo_id' => $idAnolectivo,
-        //     'escola_proveniencia_id' => $idEscola
-        // ];
-        // $candidato = CandidatoController::store($dadosCandidato);
-        // if(!$candidato)
-        // {
-        //     $msg="Lamentamos! Dados não cadastrado, tente este processo mais tarde...";
-        //     return redirect()->back()->with("ErroCadastro",$msg);
-        // }
+        $idAnolectivo = $this->pegarAnoLectivo();
+        if(!$idAnolectivo)
+        {
+            $msg="Lamentamos! Dados não cadastrado, tente este processo mais tarde...";
+            return redirect()->back()->with("ErroCadastro",$msg);
+        }
 
-        // for($i = 1; $i <= 4; $i++)
-        // {
-        //     $id[$i] = $this->pegarIdCurso($request->input('curso' . $i));
+        $dadosCandidato=[
+            //Dados do candidato
+            'nome_pai_cand'=>$request->nome_pai_cand,
+            'nome_mae_cand'=>$request->nome_mae_cand,
+            'naturalidade_cand'=>$request->naturalidade_cand,
+            'status' => "Pendente",
+            'pessoa_id' => $idPessoa,
+            'ano_lectivo_id' => $idAnolectivo,
+            'escola_proveniencia_id' => $idEscola
+        ];
+        $candidato = CandidatoController::store($dadosCandidato);
+        if(!$candidato)
+        {
+            $msg="Lamentamos! Dados não cadastrado, tente este processo mais tarde...";
+            return redirect()->back()->with("ErroCadastro",$msg);
+        }
 
-        //     if(!$id[$i])
-        //     {
-        //         $msg="Lamentamos! Dados não cadastrado, tente este processo mais tarde...";
-        //         return redirect()->back()->with("ErroCadastro",$msg);
-        //     }
-        // }
+        for($i = 1; $i <= 4; $i++)
+        {
+            $id[$i] = $this->pegarIdCurso($request->input('curso' . $i));
 
-        // $candCurso = [
-        //     'curso_id' => $id,
-        //     'candidato_id' => $candidato->candidato_id
-        // ];
-        // $cursos = CandidatoCursoController::store($candCurso);
-        // if(!$cursos)
-        // {
-        //     $msg="Lamentamos! Dados não cadastrado, tente este processo mais tarde...";
-        //     return redirect()->back()->with("ErroCadastro",$msg);
-        // }
+            if(!$id[$i])
+            {
+                $msg="Lamentamos! Dados não cadastrado, tente este processo mais tarde...";
+                return redirect()->back()->with("ErroCadastro",$msg);
+            }
+        }
 
-        // $msg="Candidato inscrito com sucesso!";
-        // return redirect()->back()->with("Sucesso",$msg);
+        $candCurso = [
+            'curso_id' => $id,
+            'candidato_id' => $candidato->candidato_id
+        ];
+        $cursos = CandidatoCursoController::store($candCurso);
+        if(!$cursos)
+        {
+            $msg="Lamentamos! Dados não cadastrado, tente este processo mais tarde...";
+            return redirect()->back()->with("ErroCadastro",$msg);
+        }
+
+        $msg="Candidato inscrito com sucesso!";
+        return redirect()->back()->with("Sucesso",$msg);
 
     }
 }
