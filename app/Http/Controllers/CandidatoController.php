ae4a4a9b4503283e215d4c862fec5eb17b09f812
Marcio Miguel <dfamilia124@gmail.com>
2023-06-07 00:41:50 +0100

    Inscrição

--- app/Http/Controllers/CandidatoController.php
+++ app/Http/Controllers/CandidatoController.php
@@ -76,7 +76,7 @@ class CandidatoController extends Controller
 
         $candidatos = Candidato::with( 'pessoa', 'escola', 'ano_lectivo')
         ->where('ano_lectivo_id', AnoLectivoController::pegarIdAnoLectivo())
-        ->get()->toarray();
+        ->get();
 
         foreach ($candidatos as &$candidato)
         {
