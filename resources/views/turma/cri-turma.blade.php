fd6e12a6cbd683ffbfd552ec89a1bf7da55bc01b
Eliseubartolomeu <eliseupereirag@gmail.com>
2023-06-29 04:03:09 +0100

    Graficos dinamicos e vagas

--- resources/views/turma/cri-turma.blade.php
+++ resources/views/turma/cri-turma.blade.php
@@ -5,12 +5,12 @@
 @section('conteudo')
 <main id="main" class="main" >
 
-    <form id="regFormh" action="" class="formulario-layout">
+    <form id="formT" method="POST" class="formulario-layout">
         <div style="text-align:center;margin-top:10px;">
-        <span class="step"></span>
+       
         </div>
 
-        <div class="tab">     
+        <div class="tabs">     
 
             <div class="row" >
                 <div class="col" style=" margin-top: 5px; margin-bottom: 5px;">
@@ -21,91 +21,63 @@
 
                 </div>
             </div>
+             
 
             <div class="form-group">
-                <input class="form-control" type="text" style=" text-align: center;" placeholder="Nome da turma" name="" oninput="this.className = ''" disabled>
-            </div>
-
-            <div class="row">
+                <input class="form-control" id="vagas-input" type="number" style=" text-align: center;" placeholder="Nome da turma" disabled>
                 
-                <div class="col-4 form-group gap-1 ">
-                    <select name="opcoes" id="opcoes" oninput="this.className = ''" class="form-select">
-                        <option selected disabled>Classe:</option>
-                        <option value="Décima">10ª</option>
-                        <option value="Décima-primeira">11ª</option>
-                        <option value="Décima-segunda">12ª</option>
-                        <option value="Décima-terceira">13ª</option>
-                    </select>
-                </div>
-                    
-                <div class="col-4 form-group gap-1 ">
-                    <select name="opcoes" id="opcoes" oninput="this.className = ''" class="form-select">
-                        <option selected disabled>Turno:</option>
-                        <option value="Manhã">Manhã</option>
-                        <option value="Tarde">Tarde</option>
-                        <option value="Noite">Noite</option>
-                    </select>
-                </div>
-
-                <div class="col">
-                    <input class="form-control" type="text" readonly="true" value="VAGAS: 50" name="" desable="" disabled>
-                </div>
             </div>
+
 
             <div class="row">
 
                 <div class="form-group col">
                     <select name="opcoes" id="opcoes" oninput="this.className = ''" class="form-select">
                         <option selected disabled>CURSO</option>
-                            <option value ="Informática">Informática</option>
-                            <option value="Desenhador projetista">Desenhador projetista</option>
-                            <option value="Técnico de Energia e Instalações Electricas">Técnico de Energia e Instalações Electricas</option>
-                            <option value="Técnico de Informática">Técnico de Informática</option>
-                            <option value="Electronica e Telecomunicação">Electronica e Telecomunicação</option>
+                        @foreach($cursos as $curso)
+                        <option value="{{ $curso->curso_id }}">{{ $curso->nome_curso }}</option>
+                        @endforeach
                     </select>
                 </div>
-
+                
                 <div class="col form-group gap-1 ">
-                    <select name="opcoes" id="opcoes" oninput="this.className = ''" class="form-select">
-                        <option disabled selected>sala:</option>
-                        <option value="01">01</option>
-                        <option value="02">02</option>
-                        <option value="03">03</option>
-                        <option value="04">04</option>
-                        <option value="05">05</option>
-                        <option value="06">06</option>
-                        <option value="07">07</option>
-                        <option value="08">08</option>
-                        <option value="09">09</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                        <option value="13">13</option>
-                        <option value="14">14</option>
-                        <option value="15">15</option>
-                        <option value="16">16</option>
-                        <option value="17">17</option>
-                        <option value="18">18</option>
-                        <option value="19">19</option>
-                        <option value="20">20</option>
-                        <option value="21">21</option>
-                        <option value="22">22</option>
-                        <option value="23">23</option>
-                        <option value="24">24</option>
+                    <select name="opcoes" id="turnoSelect" oninput="this.className = ''" class="form-select">
+                        <option selected disabled>Turno:</option>
+                        @foreach($turnos as $turno)
+                        <option value="{{ $turno->turno_id }}">{{ $turno->nome_turno }}</option>
+                        @endforeach
                     </select>
                 </div>
-            </div>
-
-            <div class="form-group">
-                <input class="form-control" type="text"  style=" text-align: center; "placeholder="Ano Lectivo" name="" oninput="this.className = ''" disabled>
             </div>
 
             <div style="text-align:center;margin-top:10px;">
                 <div>
-                    <button type="button" class="btn btn-success">Criar Turma</button>
+                    <button type="submit" class="btn btn-success">Criar Turma</button>
                 </div>
             </div>    
         </div>
     </form>
+    <script>
+        const turnoSelect = document.getElementById('turnoSelect');
+        const vagasInput = document.getElementById('vagas-input');
+    
+        turnoSelect.addEventListener('change', updateVagasInput);
+    
+        function updateVagasInput() {
+            const turnoId = turnoSelect.value;
+            const vagas = @json($vagas);
+    
+            let totalVagas = 0;
+    
+            for (let i = 0; i < vagas.length; i++) {
+                if (vagas[i].turnoId == turnoId) {
+                    totalVagas = vagas[i].totalVagas;
+                    break;
+                }
+            }
+    
+            vagasInput.value = totalVagas;
+        }
+    </script>
 </main>
 @endsection
