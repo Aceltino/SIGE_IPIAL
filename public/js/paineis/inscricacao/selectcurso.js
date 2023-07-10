c48135f73b78f0026ac209bafe15857cc1a45600
Marcio Miguel <dfamilia124@gmail.com>
2023-07-10 02:13:14 +0100

    Tudo update

--- public/js/paineis/inscricacao/selectcurso.js
+++ public/js/paineis/inscricacao/selectcurso.js
@@ -1,24 +1,18 @@
 $(document).ready(function() {
   // Quando uma opção é selecionada em qualquer select
-  $(document).on("change", "select#select", function() {
+  $(document).on("change", "select[id^='cursos']", function() {
     var selectedOption = $(this).val();
     
-    // Desabilita o select selecionado
-    $(this).prop("disabled", true);
+    // Bloqueia a opção selecionada em outros selects
+    $("select[id^='cursos']").not(this).find("option[value='" + selectedOption + "']").prop("disabled", true);
     
-    // Habilita os outros selects
-    $("select#select").not(this).prop("disabled", false);
-    
-    // Desabilita a opção selecionada em outros selects
-    $("select#select option[value='" + selectedOption + "']").not(":selected").prop("disabled", true);
+    // Remove a opção selecionável do select atual
+    $(this).find("option").not(":selected").prop("disabled", true);
   });
 
   // Ao enviar o formulário
   $("form").on("submit", function() {
-    // Habilita todos os selects
-    $("select#select").prop("disabled", false);
-    
-    // Habilita as opções selecionadas
-    $("select#select option:selected").prop("disabled", false);
+    // Habilita todas as opções
+    $("select[id^='cursos'] option").prop("disabled", false);
   });
 });
--- resources/views/matricula/matriculas.blade.php
+++ resources/views/matricula/matriculas.blade.php
@@ -39,8 +39,9 @@
     </div>
 
       <div class="col-lg-2 no-print">
-        <select class="btn-sel form-select no-print">
+        <select class="btn-sel form-select no-print" id="filtro5">
           <option class="no-print" selected disabled>Estado</option>
+          <option class="no-print" value="Todos">Todos</option>
           <option class="no-print" >Em curso</option>
           <option class="no-print" >RPF</option>
           <option class="no-print" >Anulou a Matricula</option>
@@ -61,6 +62,7 @@
       <div class="col-lg-2 no-print">
         <select class="btn-sel form-select no-print" id="filtro">
           <option class="no-print" selected disabled>Curso</option>
+          <option  class="no-print" value="Todos">Todos</option>
           @foreach($cursos as $curso)
           <option class="no-print" value="{{$curso['nome_curso']}}">{{$curso['nome_curso']}}</option>
           @endforeach
