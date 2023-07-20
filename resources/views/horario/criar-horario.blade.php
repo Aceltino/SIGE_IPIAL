52dfeca8ccc7dfd47aa4019e5eb7a2049ced1da0
Eurico Dande <euricodande12@gmail.com>
2023-07-20 17:15:59 +0100

    Avanços.

--- resources/views/horario/criar-horario.blade.php
+++ resources/views/horario/criar-horario.blade.php
@@ -17,25 +17,13 @@
 
         <div class="tab">
             <h2 class="text-uppercase text-center my-3 color-blue-principal">Dados do horário</h2>
-            <h6 class="text-uppercase text-center my-4 color-blue-principal">Selecione a turma e a respectiva sala.</h6>
+            <h6 class="text-uppercase text-center my-4 color-blue-principal">Selecione a turma.</h6>
 
             <div class="row mb-3">
                 <div class="col">
                     <select oninput="this.className = ''" name='turma' class="form-select" id="horario-select-turma">
                         <optgroup id="optgroup-turma" label="Turma">
                             
-                        </optgroupb>
-                    </select>
-                </div>
-            </div>
-
-            <div class="row mb-3">
-                <div class="col">
-                    <select oninput="this.className = ''" name='turma' class="form-select" id="horario-select-turma">
-                        <optgroup label="Salas">
-                            <option value="Sala 7">Sala 1</option>
-                            <option value="Sala 2">Sala 2</option>
-                            <option value="Sala 3">Sala 3</option>
                         </optgroup>
                     </select>
                 </div>
@@ -47,210 +35,12 @@
           <h6 class="text-uppercase text-center my-4 color-blue-principal">Selecione os professores para as disciplinas apresentadas.</h6>
           
           <div class="row d-flex align-items-center mb-3">
-                <div class="col">
+                <div class="col" id="div-disciplinas-no-horario">
                     <input type="text" value="Língua Portuguesa" name="disciplina1" oninput="this.className = ''" style="color: var(--cor-label-inativa);background-color: var(--cor-fundo-disable); text-align: center;" disabled>
                 </div>
 
-                <div class="col">
+                <div class="col" id="div-professores-no-horario">
                     <select oninput="this.className = ''" name='professor1' class="form-select">
-                        <optgroup label="Professores">
-                        <option value="Sungo Afonso">Sungo Afonso</option>
-                        <option value="Brigith Barros">Brigith Barros</option>
-                        <option value="Sivi Lando">Sivi Lando</option>
-                        <option value="Anatoli Lussati">Anatoli Lussati</option>
-                        <option value="Rosa António">Rosa António</option>
-                        </optgroup>
-                    </select>
-                </div>
-            </div>
-
-            <div class="row d-flex align-items-center mb-3">
-                <div class="col">
-                    <input type="text" value="Língua Portuguesa" name="" oninput="this.className = ''" style="color: var(--cor-label-inativa);background-color: var(--cor-fundo-disable); text-align: center;" disabled>
-                </div>
-
-                <div class="col">
-                    <select oninput="this.className = ''" class="form-select">
-                        <optgroup label="Professores">
-                        <option value="Sungo Afonso">Sungo Afonso</option>
-                        <option value="Brigith Barros">Brigith Barros</option>
-                        <option value="Sivi Lando">Sivi Lando</option>
-                        <option value="Anatoli Lussati">Anatoli Lussati</option>
-                        <option value="Rosa António">Rosa António</option>
-                        </optgroup>
-                    </select>
-                </div>
-            </div>
-
-            <div class="row d-flex align-items-center mb-3">
-                <div class="col">
-                    <input type="text" value="Língua Portuguesa" name="" oninput="this.className = ''" style="color: var(--cor-label-inativa);background-color: var(--cor-fundo-disable); text-align: center;" disabled>
-                </div>
-
-                <div class="col">
-                    <select oninput="this.className = ''" class="form-select">
-                        <optgroup label="Professores">
-                        <option value="Sungo Afonso">Sungo Afonso</option>
-                        <option value="Brigith Barros">Brigith Barros</option>
-                        <option value="Sivi Lando">Sivi Lando</option>
-                        <option value="Anatoli Lussati">Anatoli Lussati</option>
-                        <option value="Rosa António">Rosa António</option>
-                        </optgroup>
-                    </select>
-                </div>
-            </div>
-
-            <div class="row d-flex align-items-center mb-3">
-                <div class="col">
-                    <input type="text" value="Língua Portuguesa" name="" oninput="this.className = ''" style="color: var(--cor-label-inativa);background-color: var(--cor-fundo-disable); text-align: center;" disabled>
-                </div>
-
-                <div class="col">
-                    <select oninput="this.className = ''" class="form-select">
-                        <optgroup label="Professores">
-                        <option value="Sungo Afonso">Sungo Afonso</option>
-                        <option value="Brigith Barros">Brigith Barros</option>
-                        <option value="Sivi Lando">Sivi Lando</option>
-                        <option value="Anatoli Lussati">Anatoli Lussati</option>
-                        <option value="Rosa António">Rosa António</option>
-                        </optgroup>
-                    </select>
-                </div>
-            </div>
-
-            <div class="row d-flex align-items-center mb-3">
-                <div class="col">
-                    <input type="text" value="Língua Portuguesa" name="" oninput="this.className = ''" style="color: var(--cor-label-inativa);background-color: var(--cor-fundo-disable); text-align: center;" disabled>
-                </div>
-
-                <div class="col">
-                    <select oninput="this.className = ''" class="form-select">
-                        <optgroup label="Professores">
-                        <option value="Sungo Afonso">Sungo Afonso</option>
-                        <option value="Brigith Barros">Brigith Barros</option>
-                        <option value="Sivi Lando">Sivi Lando</option>
-                        <option value="Anatoli Lussati">Anatoli Lussati</option>
-                        <option value="Rosa António">Rosa António</option>
-                        </optgroup>
-                    </select>
-                </div>
-            </div>
-
-            <div class="row d-flex align-items-center mb-3">
-                <div class="col">
-                    <input type="text" value="Língua Portuguesa" name="" oninput="this.className = ''" style="color: var(--cor-label-inativa);background-color: var(--cor-fundo-disable); text-align: center;" disabled>
-                </div>
-
-                <div class="col">
-                    <select oninput="this.className = ''" class="form-select">
-                        <optgroup label="Professores">
-                        <option value="Sungo Afonso">Sungo Afonso</option>
-                        <option value="Brigith Barros">Brigith Barros</option>
-                        <option value="Sivi Lando">Sivi Lando</option>
-                        <option value="Anatoli Lussati">Anatoli Lussati</option>
-                        <option value="Rosa António">Rosa António</option>
-                        </optgroup>
-                    </select>
-                </div>
-            </div>
-
-            <div class="row d-flex align-items-center mb-3">
-                <div class="col">
-                    <input type="text" value="Língua Portuguesa" name="" oninput="this.className = ''" style="color: var(--cor-label-inativa);background-color: var(--cor-fundo-disable); text-align: center;" disabled>
-                </div>
-
-                <div class="col">
-                <select oninput="this.className = ''" class="form-select">
-                        <optgroup label="Professores">
-                        <option value="Sungo Afonso">Sungo Afonso</option>
-                        <option value="Brigith Barros">Brigith Barros</option>
-                        <option value="Sivi Lando">Sivi Lando</option>
-                        <option value="Anatoli Lussati">Anatoli Lussati</option>
-                        <option value="Rosa António">Rosa António</option>
-                        </optgroup>
-                    </select>
-                </div>
-            </div>
-
-            <div class="row d-flex align-items-center mb-3">
-                <div class="col">
-                    <input type="text" value="Língua Portuguesa" name="" oninput="this.className = ''" style="color: var(--cor-label-inativa);background-color: var(--cor-fundo-disable); text-align: center;" disabled>
-                </div>
-
-                <div class="col">
-                    <select oninput="this.className = ''" class="form-select">
-                        <optgroup label="Professores">
-                        <option value="Sungo Afonso">Sungo Afonso</option>
-                        <option value="Brigith Barros">Brigith Barros</option>
-                        <option value="Sivi Lando">Sivi Lando</option>
-                        <option value="Anatoli Lussati">Anatoli Lussati</option>
-                        <option value="Rosa António">Rosa António</option>
-                        </optgroup>
-                    </select>
-                </div>
-            </div>
-
-            <div class="row d-flex align-items-center mb-3">
-                <div class="col">
-                    <input type="text" value="Língua Portuguesa" name="" oninput="this.className = ''" style="color: var(--cor-label-inativa);background-color: var(--cor-fundo-disable); text-align: center;" disabled>
-                </div>
-
-                <div class="col">
-                    <select oninput="this.className = ''" class="form-select">
-                        <optgroup label="Professores">
-                        <option value="Sungo Afonso">Sungo Afonso</option>
-                        <option value="Brigith Barros">Brigith Barros</option>
-                        <option value="Sivi Lando">Sivi Lando</option>
-                        <option value="Anatoli Lussati">Anatoli Lussati</option>
-                        <option value="Rosa António">Rosa António</option>
-                        </optgroup>
-                    </select>
-                </div>
-            </div>
-
-            <div class="row d-flex align-items-center mb-3">
-                <div class="col">
-                    <input type="text" value="Língua Portuguesa" name="" oninput="this.className = ''" style="color: var(--cor-label-inativa);background-color: var(--cor-fundo-disable); text-align: center;" disabled>
-                </div>
-
-                <div class="col">
-                <select oninput="this.className = ''" class="form-select">
-                        <optgroup label="Professores">
-                        <option value="Sungo Afonso">Sungo Afonso</option>
-                        <option value="Brigith Barros">Brigith Barros</option>
-                        <option value="Sivi Lando">Sivi Lando</option>
-                        <option value="Anatoli Lussati">Anatoli Lussati</option>
-                        <option value="Rosa António">Rosa António</option>
-                        </optgroup>
-                    </select>
-                </div>
-            </div>
-
-            <div class="row d-flex align-items-center mb-3">
-                <div class="col">
-                    <input type="text" value="Língua Portuguesa" name="" oninput="this.className = ''" style="color: var(--cor-label-inativa);background-color: var(--cor-fundo-disable); text-align: center;" disabled>
-                </div>
-
-                <div class="col">
-                <select oninput="this.className = ''" class="form-select">
-                        <optgroup label="Professores">
-                        <option value="Sungo Afonso">Sungo Afonso</option>
-                        <option value="Brigith Barros">Brigith Barros</option>
-                        <option value="Sivi Lando">Sivi Lando</option>
-                        <option value="Anatoli Lussati">Anatoli Lussati</option>
-                        <option value="Rosa António">Rosa António</option>
-                        </optgroup>
-                    </select>
-                </div>
-            </div>
-
-            <div class="row d-flex align-items-center mb-3">
-                <div class="col">
-                    <input type="text" value="Língua Portuguesa" name="" oninput="this.className = ''" style="color: var(--cor-label-inativa);background-color: var(--cor-fundo-disable); text-align: center;" disabled>
-                </div>
-
-                <div class="col">
-                    <select oninput="this.className = ''" class="form-select">
                         <optgroup label="Professores">
                         <option value="Sungo Afonso">Sungo Afonso</option>
                         <option value="Brigith Barros">Brigith Barros</option>
